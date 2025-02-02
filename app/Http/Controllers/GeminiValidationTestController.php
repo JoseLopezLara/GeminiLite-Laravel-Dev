526c89c8aa3c8ee8dab0420b1fedadda7c2c2aad
Jonathan Serafin Contreras Malfavon <iscjonathanserafin@gmail.com>
2025-02-02 09:40:55 -0600

    Change max output tokens to prevent errors from thinking model

--- app/Http/Controllers/GeminiValidationTestController.php
+++ app/Http/Controllers/GeminiValidationTestController.php
@@ -32,54 +32,54 @@ class GeminiValidationTestController extends Controller
             $testCases = [
                 'valid_values' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 0.8,
                     'top_k' => 20,
                 ],
                 'invalid_top_k' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 0.8,
                     'top_k' => 50, // Invalid topK
                 ],
                 'invalid_top_p' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 1.5, // Invalid topP
                     'top_k' => 20,
                 ],
                 'missing_required_field' => [
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 0.8,
                     'top_k' => 20,
                 ],
                  'null_top_k' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 0.8,
                     'top_k' => null,
                 ],
                 'null_top_p' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => null,
                     'top_k' => 20,
                 ],
                 'no_top_k' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 0.5,
                     'top_p' => 0.8,
                 ],
                 'invalid_temperature' => [
                     'prompt' => 'Test prompt',
-                    'max_tokens' => 100,
+                    'max_tokens' => 8192,
                     'temperature' => 3.0, // Invalid temperature
                     'top_p' => 0.8,
                     'top_k' => 20,
